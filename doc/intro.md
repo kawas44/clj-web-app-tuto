1a41997aad7dc6f23b2c40f450871214c824e5cb
kawas44 <kawas44@gmail.com>
2015-08-20 23:52:01 +0200

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,1 @@
+# Introduction to clj-web-app-tuto
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-web-app-tuto "0.1.0-SNAPSHOT"
+  :description "Clojure web app tuto"
+  :url "http://fix.me/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
