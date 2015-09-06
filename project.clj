649bd9fb525f4c022fd429ccf2e9c3bd684685a3
kawas44 <kawas44@gmail.com>
2015-09-06 19:01:13 +0200

    Add dev profiles with custom plugins.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://fix.me/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:plugins [[lein-pprint "1.1.2"]]
+                   :dependencies []}})
