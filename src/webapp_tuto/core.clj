4f1e9354c6d1d55bbe0b1d1723bce4449f359cbd
kawas44 <kawas44@gmail.com>
2015-09-07 01:21:34 +0200

    Use components library to wire services.
    More about commponents lib https://github.com/stuartsierra/component

--- src/webapp_tuto/core.clj
+++ src/webapp_tuto/core.clj
@@ -1,11 +1,46 @@
 (ns webapp-tuto.core
-  (:require [org.httpkit.server :as hks]))
+  (:require [com.stuartsierra.component :as sc]
+            [org.httpkit.server :as hks]))
+
 
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello HTTP-KIT!"})
 
+
+(defn- start-http-server [handler host port]
+  (hks/run-server app {:ip host :port port}))
+
+(defrecord HTTPServer [host port]
+  sc/Lifecycle
+
+  (start [this]
+    (println (format "Starting HTTP server on %s:%s" host port))
+    (assoc this :httpserver (start-http-server app host port)))
+
+  (stop [this]
+    (println "Stoping HTTP server")
+    (let [stop-httpserver-fn (get this :httpserver (constantly nil))]
+      (stop-httpserver-fn))
+    (assoc this :httpserver nil)))
+
+
+(defrecord TutoCore [httpserver]
+  sc/Lifecycle
+  (start [this]
+    (println "Staring TutoCore"))
+  (stop [this]
+    (println "Stopping TutoCore")))
+
+
+(defn create-system []
+  (sc/system-map
+    :httpserver (->HTTPServer "localhost" 8000)
+    :app (sc/using
+           (map->TutoCore {})
+           [:httpserver])))
+
+
 (defn -main [& args]
-  (hks/run-server app {:port 8080})
-  (println "Server started on localhost:8080"))
+  (.start (create-system)))
