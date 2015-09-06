a7e981a02eb995802d4480a724184ff614e35c7b
kawas44 <kawas44@gmail.com>
2015-09-06 22:46:05 +0200

    Remove unused code.

--- src/webapp_tuto/core.clj
+++ src/webapp_tuto/core.clj
@@ -1,11 +1,6 @@
 (ns webapp-tuto.core
   (:require [org.httpkit.server :as hks]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
